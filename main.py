79885e92150a7ebe7289636a0efe50d292c90755
Asinsa <asinsa.g@gmail.com>
2021-12-15 20:47:31 +0000

    Getting the links for the different datasets from 1 year of NHANES

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+URL = "https://wwwn.cdc.gov/nchs/nhanes/continuousnhanes/default.aspx?BeginYear=2017"
+page = requests.get(URL)
 
+soup = BeautifulSoup(page.content, "html.parser")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+dataset = soup.find('ul', {'class': 'mb-0 block-list'})
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for data in dataset:
+    newsoup = BeautifulSoup(str(data), 'html.parser')
+    links = newsoup.findAll('a', {'class': 'list-title td-none td-ul-hover'})
+    for link in links:
+        print(link.get("href"))
