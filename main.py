c0168717bea741df8603121e94a5c40093fb123a
Asinsa <asinsa.g@gmail.com>
2022-04-15 11:47:08 +0100

    Changed window size and added icon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("Pattern Finder")
+window.geometry(f"{window.winfo_screenwidth()}x{window.winfo_screenheight()}")
+window.iconbitmap("icon.ico")
 
 window.mainloop()
